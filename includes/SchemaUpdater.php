ed9a6e21de6fc40427073c25e17b12ffb03591ad
mekavehamichlol <mekavehamichlol@gmail.com>
2024-03-19 14:48:59 +0200

    add extension table

--- includes/SchemaUpdater.php
+++ includes/SchemaUpdater.php
@@ -10,10 +10,15 @@ class SchemaUpdater implements LoadExtensionSchemaUpdatesHook {
 	/**
 	 * @param DatabaseUpdater $updater
 	 */
-	public function onLoadExtensionSchemaUpdates( $updater ) {
+	public function onLoadExtensionSchemaUpdates($updater) {
 		$type = $updater->getDB()->getType();
-		$updater->addExtensionTable( 'aspaklarya_lockdown_pages',
-			__DIR__ . '/../dbPatches/' . $type . '/tables-generated.sql' );
-
+		$updater->addExtensionTable(
+			'aspaklarya_lockdown_pages',
+			__DIR__ . '/../dbPatches/' . $type . '/tables-generated.sql'
+		);
+		$updater->addExtensionTable(
+			'aspaklarya_lockdown_bad_words',
+			__DIR__ . '/../dbPatches/' . $type . '/bad_words.sql'
+		);
 	}
 }
