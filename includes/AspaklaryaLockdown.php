3d027be9aeef5efd44963b3eb7a765bb7299fb0a
mekavehamichlol <mekavehamichlol@gmail.com>
2024-05-16 14:54:34 +0300

    +1

--- includes/AspaklaryaLockdown.php
+++ includes/AspaklaryaLockdown.php
@@ -348,7 +348,7 @@ class AspaklaryaLockdown implements
 			foreach ( $res as $row ) {
 				if ( !isset( $regulars[$row->page_id] ) ) {
 					$regulars[$row->page_id] = $linkcolour_ids[$row->rd_from];
-					unset( $redirects[$row->rd_from] );
+					// unset( $redirects[$row->rd_from] );
 				}
 			}
 			unset( $res );
@@ -362,7 +362,7 @@ class AspaklaryaLockdown implements
 			->fetchResultSet();
 		
 		foreach ($res as $row) {
-			$colours[$regulars[$row->al_page_id]] .= $row->al_read_allowed == "1" ? 'aspaklarya-edit-locked' : 'aspaklarya-read-locked';
+			$colours[$regulars[$row->al_page_id]] .= $row->al_read_allowed == "1" ? 'aspaklarya-edit-locked' : 'aspaklarya-read-locked'  . implode(' ', $redirects);
 			if (!empty($redirects) && isset($redirects[$row->al_page_id])) {
 				$colours[$redirects[$row->al_page_id]] .= $colours[$regulars[$row->al_page_id]];
 				unset($redirects[$row->al_page_id]);
