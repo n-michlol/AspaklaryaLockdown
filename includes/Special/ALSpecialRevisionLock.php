4e18c359f43214a2f61469735c9902f6abb8f53e
mekavehamichlol <mekavehamichlol@gmail.com>
2024-05-27 21:37:44 +0300

    try

--- includes/Special/ALSpecialRevisionLock.php
+++ includes/Special/ALSpecialRevisionLock.php
@@ -363,7 +363,7 @@ class ALSpecialRevisionLock extends UnlistedSpecialPage {
 				->setSubmitText( $this->msg( 'revlock-submit', $numRevisions )->text() )
 				->setSubmitName( 'wpSubmit' )
 				->setWrapperLegend( $this->msg( 'revlock-legend' )->text() )
-				->setAction( $this->getPageTitle()->getLocalURL( [ 'action' => 'revisionlock' ] ) )
+				->setAction( $this->getTitleFor( $this->getName() )->getLocalURL( [ 'action' => 'submit'] ) )
 				->prepareForm();
 			// Show link to edit the dropdown reasons
 			if ( $this->permissionManager->userHasRight( $this->getUser(), 'editinterface' ) ) {
