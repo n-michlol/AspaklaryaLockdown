1654055c37a0e520f6a604aa25b517332751f3af
mekavehamichlol <mekavehamichlol@gmail.com>
2024-06-17 12:52:49 +0300

    +1

--- includes/Hooks/AspaklaryaLockdown.php
+++ includes/Hooks/AspaklaryaLockdown.php
@@ -565,14 +565,14 @@ class AspaklaryaLockdown implements
 		if ($module instanceof ApiQueryInfo) {
 			$params = $module->extractRequestParams();
 			$result = $module->getResult();
-			if( !isset( $params[ 'inprop' ]) || $params['inprop'] === null || !is_array($params['inprop'])) {
-				$result->addValue( [ 'query' ], 'allevel', 'mess');
-				return;
-			}
-			if( !in_array( 'allevel', $params[ 'inprop' ] ) ) {
-				$result->addValue( [ 'query' ], 'allevel', 'mess1');
-				return;
-			}
+			// if( !isset( $params[ 'inprop' ]) || $params['inprop'] === null || !is_array($params['inprop'])) {
+			// 	$result->addValue( [ 'query' ], 'allevel', 'mess');
+			// 	return;
+			// }
+			// if( !in_array( 'allevel', $params[ 'inprop' ] ) ) {
+			// 	$result->addValue( [ 'query' ], 'allevel', 'mess1');
+			// 	return;
+			// }
 			// $result = $module->getResult();
 			$data = (array)$result->getResultData( [ 'query', 'pages' ], [ 'Strip' => 'all' ] );
 			if ( !$data ) {
