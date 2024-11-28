5616b1a6cf1257ff98637e4a28fe3839d2722d38
mekavehamichlol <mekavehamichlol@gmail.com>
2024-11-28 22:24:34 +0200

    add server side template block thanx to moti

--- includes/Hooks/AspaklaryaLockdown.php
+++ includes/Hooks/AspaklaryaLockdown.php
@@ -76,7 +76,23 @@ class AspaklaryaLockdown implements
 		$mannager = new Main( $this->loadBalancer, $this->cache, $title, $user );
 		if ( !$mannager->isUserAllowedToEdit() ) {
 			$out->redirect( $title->getLocalURL() );
+			return true;
 		}
+		if ( !$user->isAllowed( 'aspaklarya-edit-locked' ) ) {
+			$templates = $editor->getTemplates();
+			$problematicTemplates = [
+				35592, // תבנית:סינון/שורה	
+				32342, // 'סינון/פסקה',
+				23612, // 'תמונה חילופית',
+			];
+			foreach ( $templates as $template ) {
+				if ( in_array( $template->getId(), $problematicTemplates, true ) ) {
+					$out->redirect( $title->getLocalURL() );
+					return true;
+				}
+			}
+		}
+		
 	}
 
 	/**
